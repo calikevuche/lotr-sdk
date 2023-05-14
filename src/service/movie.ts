ebf6d505e279bf667a41e55901e5ef20366728a6
uchenna kevin <uchevt@vt.edu>
2023-05-14 15:24:08 +0300

    More functionality into base, movie, and quote for get basic get methods

--- src/service/movie.ts
+++ src/service/movie.ts
@@ -1,5 +1,14 @@
 import {BaseGetSrvc} from "./base";
+import {Quote} from "../type/quote";
 
 export class MovieService extends BaseGetSrvc {
-  private readonly resrcUrl = 'movie';
+  private readonly rsrcUrl = 'movie';
+  public async getQuotes(movieId: string, query?: Query): Promise<Response<Quote>> {
+    const url = `${this.resourceUrl}/${movieId}/quote${query ? `?${stringify(query)}` : ''}`;
+
+    return await this.client.get(url);
+  }
+  protected getRsrcName(): string {
+    return this.rsrcUrl;
+  }
 }
